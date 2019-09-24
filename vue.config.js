45d561eab4deba86a10ec12c2c29a8797de1e700
wangyangsong <yansong_wang@foxmail.com>
2019-09-24 17:45:51 +0800

    init project base on vue3.9

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  publicPath: '/',
+  outputDir: process.env.outputDir, // 打包的目录
+  lintOnSave: true, // 在保存时校验格式
+  productionSourceMap: false, // 生产环境是否生成 SourceMap
+  devServer: {
+    open: true, // 启动服务后是否打开浏览器
+    host: 'localhost',
+    port: 8088, // 服务端口
+    https: false,
+    hotOnly: false,
+    proxy: null, // 设置代理
+    before: app => {}
+  }
+}
